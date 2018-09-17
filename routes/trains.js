b1f92ddc03327debc24712d129875e5c3856b245
Michael Snow <michaelsnow@Michaels-MacBook-Pro.local>
2018-09-18 00:06:44 +0100

    Initial build of app

---
+++ routes/trains.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var Rail = require('national-rail-darwin')
+const DARWIN_TOKEN = '744c2b06-42f9-4261-b0a3-159b96d3d931';
+var rail = new Rail(DARWIN_TOKEN) // or -> new Rail(DARWIN_TOKEN)
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+
+  rail.getFastestDeparture('DYP', 'MOG', {}, function (err, result) {
+    //do stuff
+    if (result) {
+      const { trainServices } = result;
+      const fastestService = trainServices[0];
+      res.json({ trainTime: fastestService.sta, expected: fastestService.eta });
+    }
+
+  });
+
+});
+
+module.exports = router;
